c6432ea802367331b846150b6b66d35a96eec2f7
unknown <ffarzam_1992@yahoo.com>
2023-12-03 07:59:29 +0330

    chore: add backend authentication class

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -103,4 +103,21 @@ class RefreshTokenAuthentication(AbstractTokenAuthentication):
         return user, payload
 
 
+class AuthBackend(ModelBackend):
 
+    def authenticate(self, request, email=None, password=None, **kwargs):
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return None
+
+        if user.check_password(password):
+            return user
+        else:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
